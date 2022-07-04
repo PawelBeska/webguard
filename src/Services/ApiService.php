474df4c23aff2802bb6f67da1f28a4eafae4af00
Paweł Beśka <raz4r@interia.pl>
2022-07-04 18:39:17 +0200

    Update ApiService.php

--- src/Services/ApiService.php
+++ src/Services/ApiService.php
@@ -4,6 +4,7 @@ namespace WebGuard\Services;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
 
 class ApiService
 {
@@ -25,9 +26,9 @@ class ApiService
     protected function makeCall(string $method, string $url, array $body = [], array $headers = [],)
     {
         try {
-            return json_decode(
-                $this->client->request($method, $url, [
-                ], $body)->getBody());
+            return json_decode($this->client->request($method, $url, [
+                RequestOptions::JSON => $body,
+            ])->getBody(), false, 512, JSON_THROW_ON_ERROR);
         } catch (GuzzleException $e) {
 
             return $e->getMessage();
