c3ffe32646e443250612f4a38f302dd050f66289
Paweł Beśka <raz4r@interia.pl>
2022-07-04 19:25:56 +0200

    Update WebGuard.php

--- src/Facades/WebGuard.php
+++ src/Facades/WebGuard.php
@@ -6,6 +6,9 @@ namespace WebGuard\Facades;
 use Illuminate\Support\Facades\Facade;
 use WebGuard\Services\CaptchaService;
 
+/**
+ * @method static bool validate(string $token, string $input)
+ */
 class WebGuard extends Facade
 {
     /**
